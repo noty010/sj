bbc5e156c59cba641acaabecaa4de6298e22b485
Daniel Souza <viniciusdanielsalves@gmail.com>
2025-08-22 17:46:47 +0000

    (feat): The setup function, setup(cnx), is ready to be imported to main.py and used with any connection cnx

--- model/setup.py
+++ model/setup.py
@@ -1,34 +1,21 @@
 #Run 'CREATE DATABASE tccsj' in MySQL Workbench before running setup.py
 
-import mysql.connector
+def setup(cnx):
+    def parse(cmdlist, file):
+        with open(file, "r", encoding="utf-8") as f:
+            sql = f.read()
+        rawcmd = sql.split(';')
+        for cmd in rawcmd:
+            cleancmd = cmd.strip()
+            if cleancmd: cmdlist.append(cleancmd)
 
-cnx = mysql.connector.connect(
-    host = '127.0.0.1',
-    user = 'root',
-    password = 'aluno',
-    database = 'tccsj'
-)
-
-setup = []
-feed = []
-
-def parse(cmdlist, file):
-    with open(file, "r", encoding="utf-8") as f:
-        sql = f.read()
-    rawcmd = sql.split(';')
-    for cmd in rawcmd:
-        cleancmd = cmd.strip()
-        if cleancmd:
-            cmdlist.append(cleancmd)
-
-def setup():
-    cursor = cnx.cursro()
-    for command in parse(setup, "database/create.sql"):
+    setup = []
+    feed = []
+    cursor = cnx.cursor()
+    for command in parse(setup, "create.sql"):
         cursor.execute(command)
         cursor.commit()
-    for command in parse(feed, "database/feed.sql"):
+    for command in parse(feed, "feed.sql"):
         cursor.execute.(command)
         cursor.commit()
     cursor.close()
-
-setup()
