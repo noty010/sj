86ea1437329306fdbd54de0a87f5512e70fdd9d3
Daniel Souza <viniciusdanielsalves@gmail.com>
2025-08-22 18:13:02 +0000

    (feat): Adds function getobjects, which selects objects from a given table

--- model/db_utils.py
+++ model/db_utils.py
@@ -1,7 +1,7 @@
 #Run 'CREATE DATABASE tccsj' in MySQL Workbench before running setup.py
 
-def setup(cnx):
-    def parse(cmdlist, file):
+def setup(cnx): # Inicializa o banco de dados para rodar a API
+    def parse(cmdlist, file): # Lê os comandos em MySQL e executa
         with open(file, "r", encoding="utf-8") as f:
             sql = f.read()
         rawcmd = sql.split(';')
@@ -18,4 +18,14 @@ def setup(cnx):
     for command in parse(feed, "feed.sql"):
         cursor.execute.(command)
         cursor.commit()
+    cursor.close()
+
+def getobjects(cnx, table, cod=None): # Busca objetos de uma tabela
+    cursor = cnx.cursor()
+    if cod is None:
+        cursor.execute("SELECT * FROM" % table)
+        objects = cursor.fetchall()
+    else:
+        cursor.execute("SELECT * FROM %s WHERE cod = %i" % (table, cod))
+        objects = cursor.fetchall()
     cursor.close()
