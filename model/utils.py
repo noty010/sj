4551c2593fe4bb8303aa6221ed67ed27fea1dd9c
Daniel Souza <viniciusdanielsalves@gmail.com>
2025-09-01 16:57:12 +0000

    (feat) New GET functions in utilities

--- model/utils.py
+++ model/utils.py
@@ -22,3 +22,23 @@ def feed(cnx):
         cursor.execute(command)
         cursor.commit()
     cursor.close()
+
+def getall(cnx, table):
+    cursor = cnx.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM %s" % table)
+    results = cursor.fetchall()
+    cursor.close()
+    return results
+
+def getbyid(cnx, table, code):
+    cursor = cnx.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM %s WHERE cod = %i" % (table, code))
+    results = cursor.fetchall()
+    cursor.close()
+    return results
+
+def delete(cnx, table, code):
+    cursor = cnx.cursor()
+    cursor.execute("DELETE FROM %s WHERE cod = %i" % (table, code))
+    cursor.commit()
+    cursor.close()
